3af477ba5443b1ed46991a5d2f974776f4805aa4
Christian Nunnally <christian.nunnally@ni.com>
2021-08-03 14:27:43 -0500

    added unit tests and refactored code to be more object oriented

--- test/test_slmigrate.py
+++ test/test_slmigrate.py
@@ -14,52 +14,6 @@ from slmigrate.migrationaction import MigrationAction
 from test import test_constants
 from .context import systemlinkmigrate
 
-@pytest.mark.unit
-def test_parse_arguments():
-    """TODO: Complete documentation.
-
-    :return:
-    """
-    parser = arg_handler.create_nislmigrate_argument_parser()
-    assert parser.parse_args(
-        [
-            constants.CAPTURE_ARG,
-            "--" + constants.tag.arg,
-            "--" + constants.opc.arg,
-            "--" + constants.testmonitor.arg,
-            "--" + constants.alarmrule.arg,
-            "--" + constants.opc.arg,
-            "--" + constants.asset.arg,
-            "--" + constants.repository.arg,
-            "--" + constants.userdata.arg,
-            "--" + constants.notification.arg,
-            "--" + constants.states.arg,
-        ]
-    )
-
-@pytest.mark.unit
-def test_parse_arguments():
-    """TODO: Complete documentation.
-
-    :return:
-    """
-    parser = arg_handler.create_nislmigrate_argument_parser()
-    assert parser.parse_args(
-        [
-            constants.CAPTURE_ARG,
-            "--" + constants.tag.arg,
-            "--" + constants.opc.arg,
-            "--" + constants.testmonitor.arg,
-            "--" + constants.alarmrule.arg,
-            "--" + constants.opc.arg,
-            "--" + constants.asset.arg,
-            "--" + constants.repository.arg,
-            "--" + constants.userdata.arg,
-            "--" + constants.notification.arg,
-            "--" + constants.states.arg,
-        ]
-    )
-
 
 @pytest.mark.unit
 def test_double_action_args():
