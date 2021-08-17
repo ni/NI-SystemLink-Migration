2608919a8dfe45c91063d6bfc5ead1c7bb284757
Christian Nunnally <christian.nunnally@ni.com>
2021-08-17 10:57:07 -0500

    fixing setup.py for now

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def read(file_name):
 settings = dict(
     name="nislmigrate",
     packages=["nislmigrate", "nislmigrate.plugins"],
-    scripts=["slmigrate"],
+    scripts=["slmigrate.py.py"],
     version=__version__,
     author="prestwick",
     author_email="",
