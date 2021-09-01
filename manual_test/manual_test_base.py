86559704c66f593c9a0e3e2b6db71f2feb1d4d17
Chris Pawling <chris.pawling@ni.com>
2021-09-01 09:20:15 -0500

    Add type hints

--- manual_test/manual_test_base.py
+++ manual_test/manual_test_base.py
@@ -1,11 +1,12 @@
 import argparse
 from requests.auth import HTTPBasicAuth
 import requests
+from typing import Type
 from urllib.parse import urljoin
 
 
 class ManualTestBase:
-    def __init__(self, server, username, password):
+    def __init__(self, server: str, username: str, password: str) -> None:
         """
         Constructs the manual test base class.
 
@@ -18,14 +19,14 @@ class ManualTestBase:
         self._server = server
         self._auth = HTTPBasicAuth(username, password)
 
-    def populate_data(self):
+    def populate_data(self) -> None:
         """
         Derived class should override to populate the SystemLink server with test data.
         """
 
         raise NotImplementedError
 
-    def validate_data(self):
+    def validate_data(self) -> None:
         """
         Derived class should override to validate the SystemLink server containst the test
         data added by populate_data. validate_data and populate_data can be called from
@@ -34,7 +35,7 @@ class ManualTestBase:
 
         raise NotImplementedError
 
-    def request(self, method, route, **kwargs):
+    def request(self, method: str, route: str, **kwargs) -> requests.Response:
         """
         Sends a request.
 
@@ -50,52 +51,52 @@ class ManualTestBase:
                 verify=kwargs.pop('verify', False),
                 **kwargs)
 
-    def get(self, route, **kwargs):
+    def get(self, route: str, **kwargs) -> requests.Response:
         """
         Sends a get request. See self.request for parameter details.
         """
 
         return self.request("GET", route, **kwargs)
 
-    def post(self, route, **kwargs):
+    def post(self, route: str, **kwargs) -> requests.Response:
         """
         Sends a post request. See self.request for parameter details.
         """
 
         return self.request("POST", route, **kwargs)
 
-    def put(self, route, **kwargs):
+    def put(self, route: str, **kwargs) -> requests.Response:
         """
         Sends a put request. See self.request for parameter details.
         """
 
         return self.request("PUT", route, **kwargs)
 
-    @staticmethod
-    def handle_command_line(derived_class):
-        """
-        Parses command line arguments, instantiates a test class,
-        and populates or verifies data.
 
-        :param derived_class: The test class to instantiate
-        """
+def handle_command_line(test_class: Type[ManualTestBase]) -> None:
+    """
+    Parses command line arguments, instantiates a test class,
+    and populates or verifies data.
 
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--server', '-s', required=True, help='systemlink server url. eg https://server')
-        parser.add_argument('--username', '-u', required=True, help='server username')
-        parser.add_argument('--password', '-p', required=True, help='server password.')
-        subparsers = parser.add_subparsers(dest='command', required=True)
-        subparsers.add_parser('populate', help='populate the server with test data')
-        subparsers.add_parser('validate', help='validate the data on the server matches the test data')
+    :param derived_class: The test class to instantiate
+    """
 
-        options = parser.parse_args()
-        server = options.server
-        username = options.username
-        password = options.password
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server', '-s', required=True, help='systemlink server url. eg https://server')
+    parser.add_argument('--username', '-u', required=True, help='server username')
+    parser.add_argument('--password', '-p', required=True, help='server password.')
+    subparsers = parser.add_subparsers(dest='command', required=True)
+    subparsers.add_parser('populate', help='populate the server with test data')
+    subparsers.add_parser('validate', help='validate the data on the server matches the test data')
 
-        test = derived_class(server, username, password)
+    options = parser.parse_args()
+    server = options.server
+    username = options.username
+    password = options.password
 
-        if 'populate' == options.command:
-            test.populate_data()
-        elif 'validate' == options.command:
-            test.validate_data()
+    test = test_class(server, username, password)
+
+    if 'populate' == options.command:
+        test.populate_data()
+    elif 'validate' == options.command:
+        test.validate_data()
