c635080f36ba80f22605f5fb558ede6e92b835ad
Christian Nunnally <christian.nunnally@ni.com>
2021-09-02 09:39:29 -0500

    refactor fake config

--- test/facades/test_mongo_facade.py
+++ test/facades/test_mongo_facade.py
@@ -4,10 +4,9 @@ from unittest.mock import patch, Mock
 import pytest as pytest
 from testfixtures import tempdir, TempDirectory
 
+from nislmigrate.facades import mongo_configuration
 from nislmigrate.facades.mongo_configuration import MongoConfiguration
 from nislmigrate.facades.mongo_facade import MongoFacade
-
-connection_string = "mongodb://127.17.0.5:27017/?gssapiServiceName=mongodb"
 
 
 @pytest.mark.unit
@@ -19,11 +18,10 @@ def test_mongo_facade_capture_migration_directory_created_when_it_does_not_exist
 ) -> None:
     migration_directory = os.path.join(temp_directory.path, "migration")
     assert not os.path.exists(migration_directory)
-    mongo_configuration = FakeMongoConfiguration()
-    mongo_configuration.custom_connection_string = connection_string
+    configuration = FakeMongoConfiguration()
     mongo_facade = MongoFacade()
 
-    mongo_facade.capture_database_to_directory(mongo_configuration, migration_directory, "testname.gz")
+    mongo_facade.capture_database_to_directory(configuration, migration_directory, "testname.gz")
 
     assert os.path.exists(migration_directory)
     run.verify_called()
@@ -38,11 +36,10 @@ def test_mongo_facade_capture_migration_directory_already_exists_and_empty(
 ) -> None:
     migration_directory = make_directory(temp_directory, "migration")
     assert os.path.exists(migration_directory)
-    mongo_configuration = FakeMongoConfiguration()
-    mongo_configuration.custom_connection_string = connection_string
+    configuration = FakeMongoConfiguration()
     mongo_facade = MongoFacade()
 
-    mongo_facade.capture_database_to_directory(mongo_configuration, migration_directory, "testname.gz")
+    mongo_facade.capture_database_to_directory(configuration, migration_directory, "testname.gz")
 
     assert os.path.exists(migration_directory)
     run.verify_called()
@@ -56,34 +53,11 @@ def make_directory(temp_directory: TempDirectory, name: str) -> str:
 
 class FakeMongoConfiguration(MongoConfiguration):
     def __init__(self):
-        super().__init__({})
-        self.custom_password = ""
-        self.custom_user = ""
-        self.custom_connection_string = ""
-        self.custom_port = ""
-        self.custom_collection_name = "test"
-        self.custom_host_name = ""
-
-    @property
-    def password(self) -> str:
-        return self.custom_password
-
-    @property
-    def user(self) -> str:
-        return self.custom_user
-
-    @property
-    def connection_string(self) -> str:
-        return self.custom_connection_string
-
-    @property
-    def port(self) -> str:
-        return self.custom_port
-
-    @property
-    def collection_name(self) -> str:
-        return self.custom_collection_name
-
-    @property
-    def host_name(self) -> str:
-        return self.custom_host_name
+        super().__init__({
+            mongo_configuration.MONGO_PASSWORD_CONFIGURATION_KEY: "",
+            mongo_configuration.MONGO_USER_CONFIGURATION_KEY: "",
+            mongo_configuration.MONGO_CUSTOM_CONNECTION_STRING_CONFIGURATION_KEY: "",
+            mongo_configuration.MONGO_PORT_NAME_CONFIGURATION_KEY: "",
+            mongo_configuration.MONGO_DATABASE_NAME_CONFIGURATION_KEY: "",
+            mongo_configuration.MONGO_HOST_NAME_CONFIGURATION_KEY: "",
+        })
