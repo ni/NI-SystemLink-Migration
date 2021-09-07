146635ecdb5ef200e10a77beae67aef1b00a1c45
Chris Pawling <chris.pawling@ni.com>
2021-09-07 12:26:25 -0500

    Add a fake process facade in test utilities

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,6 +1,7 @@
 from nislmigrate.facades.mongo_facade import MongoFacade
-from nislmigrate.facades.process_facade import ProcessFacade
+from nislmigrate.facades.process_facade import ProcessFacade, BackgroundProcess
 from nislmigrate.facades.system_link_service_manager_facade import SystemLinkServiceManagerFacade
+from typing import List, Optional
 
 
 class FakeServiceManager(SystemLinkServiceManagerFacade):
@@ -13,11 +14,27 @@ class FakeServiceManager(SystemLinkServiceManagerFacade):
         self.are_services_running = True
 
 
+class NoopBackgroundProcess(BackgroundProcess):
+    def __init__(self, arguments: List[str]):
+        pass
+
+    def stop(self):
+        pass
+
+
+class FakeProcessFacade(ProcessFacade):
+    def run_process(self, args: List[str]):
+        pass
+
+    def run_background_process(self, args: List[str]) -> BackgroundProcess:
+        return NoopBackgroundProcess(args)
+
+
 class FakeMongoFacade(MongoFacade):
     is_mongo_running = True
 
-    def __init__(self, process_facade: ProcessFacade):
-        super().__init__(process_facade)
+    def __init__(self, process_facade: Optional[ProcessFacade] = None):
+        super().__init__(process_facade or FakeProcessFacade())
 
     def start_mongo(self):
         self.is_mongo_running = True
