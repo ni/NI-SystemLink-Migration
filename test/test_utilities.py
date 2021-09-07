10814c7ea4f0f16df335e2e48730ea7ed1c2bbf7
Chris Pawling <chris.pawling@ni.com>
2021-09-07 15:29:54 -0500

    Move fake mongo facade to test utilities

--- test/test_utilities.py
+++ test/test_utilities.py
@@ -1,3 +1,5 @@
+from nislmigrate.facades.mongo_facade import MongoFacade
+from nislmigrate.facades.process_facade import ProcessFacade
 from nislmigrate.facades.system_link_service_manager_facade import SystemLinkServiceManagerFacade
 
 
@@ -9,3 +11,16 @@ class FakeServiceManager(SystemLinkServiceManagerFacade):
 
     def start_all_system_link_services(self) -> None:
         self.are_services_running = True
+
+
+class FakeMongoFacade(MongoFacade):
+    is_mongo_running = True
+
+    def __init__(self, process_facade: ProcessFacade):
+        super().__init__(process_facade)
+
+    def start_mongo(self):
+        self.is_mongo_running = True
+
+    def stop_mongo(self):
+        self.is_mongo_running = False
