14b38427d5f2fe0e2213d13c7858f0812aefd443
Chris Pawling <chris.pawling@ni.com>
2021-09-09 17:06:12 -0500

    Add files option to avoid copying files

--- nislmigrate/migrators/file_migrator.py
+++ nislmigrate/migrators/file_migrator.py
@@ -1,7 +1,7 @@
 import os
 
 from nislmigrate.facades.facade_factory import FacadeFactory
-from nislmigrate.extensibility.migrator_plugin import MigratorPlugin
+from nislmigrate.extensibility.migrator_plugin import MigratorPlugin, ArgumentManager
 from nislmigrate.facades.file_system_facade import FileSystemFacade
 from nislmigrate.facades.mongo_configuration import MongoConfiguration
 from nislmigrate.facades.mongo_facade import MongoFacade
@@ -15,6 +15,11 @@ DEFAULT_DATA_DIRECTORY = os.path.join(
     'FileIngestion')
 
 PATH_CONFIGURATION_KEY = 'OutputPath'
+
+_METADATA_ONLY_ARGUMENT = 'metadata-only'
+
+_METADATA_ONLY_HELP = 'When used with --files or --all, migrate only the file metadata, \
+but not the files themselves. Otherwise ignored.'
 
 
 class FileMigrator(MigratorPlugin):
@@ -41,10 +46,12 @@ class FileMigrator(MigratorPlugin):
             mongo_configuration,
             migration_directory,
             self.name)
-        file_facade.copy_directory(
-            self.__data_directory(facade_factory),
-            file_migration_directory,
-            False)
+
+        if not arguments.get(_METADATA_ONLY_ARGUMENT, False):
+            file_facade.copy_directory(
+                self.__data_directory(facade_factory),
+                file_migration_directory,
+                False)
 
     def restore(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
@@ -56,10 +63,12 @@ class FileMigrator(MigratorPlugin):
             mongo_configuration,
             migration_directory,
             self.name)
-        file_facade.copy_directory(
-            file_migration_directory,
-            self.__data_directory(facade_factory),
-            True)
+
+        if not arguments.get(_METADATA_ONLY_ARGUMENT, False):
+            file_facade.copy_directory(
+                file_migration_directory,
+                self.__data_directory(facade_factory),
+                True)
 
     def pre_restore_check(
             self,
@@ -70,6 +79,9 @@ class FileMigrator(MigratorPlugin):
         mongo_facade.validate_can_restore_database_from_directory(
             migration_directory,
             self.name)
+
+    def add_additional_arguments(self, argument_manager: ArgumentManager):
+        argument_manager.add_switch(_METADATA_ONLY_ARGUMENT, help=_METADATA_ONLY_HELP)
 
     def __data_directory(self, facade_factory):
         return self.config(facade_factory).get(PATH_CONFIGURATION_KEY, DEFAULT_DATA_DIRECTORY)
--- test/migrators/test_file_migrator.py
+++ test/migrators/test_file_migrator.py
@@ -1,5 +1,5 @@
 from nislmigrate.facades.file_system_facade import FileSystemFacade
-from nislmigrate.migrators.file_migrator import FileMigrator, DEFAULT_DATA_DIRECTORY, PATH_CONFIGURATION_KEY
+from nislmigrate.migrators.file_migrator import FileMigrator, DEFAULT_DATA_DIRECTORY, PATH_CONFIGURATION_KEY, _METADATA_ONLY_ARGUMENT
 import pytest
 from test.test_utilities import FakeFacadeFactory
 from typing import Optional
@@ -47,7 +47,7 @@ def test_file_migrator_restores_to_default_location_when_unconfigured():
 
 
 @pytest.mark.unit
-def test_file_migrator_resteores_to_configured_location():
+def test_file_migrator_restores_to_configured_location():
 
     facade_factory = FakeFacadeFactory()
     file_system_facade = FakeFileSystemFacade()
@@ -59,6 +59,34 @@ def test_file_migrator_resteores_to_configured_location():
     migrator.restore('data_dir', facade_factory, {})
 
     assert file_system_facade.last_to_directory == expected_directory
+
+
+@pytest.mark.unit
+def test_file_migrator_does_not_capture_files_when_metadata_only_is_passed():
+
+    facade_factory = FakeFacadeFactory()
+    file_system_facade = FakeFileSystemFacade()
+    facade_factory.file_system_facade = file_system_facade
+    migrator = FileMigrator()
+    expected_directory = 'custom/directory'
+
+    migrator.capture('data_dir', facade_factory, {_METADATA_ONLY_ARGUMENT: True})
+
+    assert file_system_facade.last_from_directory == None
+
+
+@pytest.mark.unit
+def test_file_migrator_does_not_restore_files_when_metadata_only_is_passed():
+
+    facade_factory = FakeFacadeFactory()
+    file_system_facade = FakeFileSystemFacade()
+    facade_factory.file_system_facade = file_system_facade
+    migrator = FileMigrator()
+    expected_directory = 'custom/directory'
+
+    migrator.restore('data_dir', facade_factory, {_METADATA_ONLY_ARGUMENT: True})
+
+    assert file_system_facade.last_to_directory == None
 
 
 class FakeFileSystemFacade(FileSystemFacade):
