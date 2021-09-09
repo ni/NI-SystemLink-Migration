bc18b4586261b385457eba5d2d2de1eafbf8e426
Chris Pawling <chris.pawling@ni.com>
2021-09-09 11:42:37 -0500

    Add arguments parameter to migrators (#69)

--- nislmigrate/migrators/file_migrator.py
+++ nislmigrate/migrators/file_migrator.py
@@ -5,6 +5,7 @@ from nislmigrate.extensibility.migrator_plugin import MigratorPlugin
 from nislmigrate.facades.file_system_facade import FileSystemFacade
 from nislmigrate.facades.mongo_configuration import MongoConfiguration
 from nislmigrate.facades.mongo_facade import MongoFacade
+from typing import Any, Dict
 
 DEFAULT_DATA_DIRECTORY = os.path.join(
     str(os.environ.get("ProgramData")),
@@ -30,7 +31,7 @@ class FileMigrator(MigratorPlugin):
     def help(self):
         return "Migrate ingested files"
 
-    def capture(self, migration_directory: str, facade_factory: FacadeFactory):
+    def capture(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
         file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
         mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
@@ -45,7 +46,7 @@ class FileMigrator(MigratorPlugin):
             file_migration_directory,
             False)
 
-    def restore(self, migration_directory: str, facade_factory: FacadeFactory):
+    def restore(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
         file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
         mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
@@ -60,7 +61,11 @@ class FileMigrator(MigratorPlugin):
             self.__data_directory(facade_factory),
             True)
 
-    def pre_restore_check(self, migration_directory: str, facade_factory: FacadeFactory) -> None:
+    def pre_restore_check(
+            self,
+            migration_directory: str,
+            facade_factory: FacadeFactory,
+            arguments: Dict[str, Any]) -> None:
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
         mongo_facade.validate_can_restore_database_from_directory(
             migration_directory,
