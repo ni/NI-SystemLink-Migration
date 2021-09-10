fc39f4af93a1dc5956a540d5135b7b0f3e5cec65
Chris Pawling <chris.pawling@ni.com>
2021-09-10 09:40:52 -0500

    File database only (#77)
    * Add files option to avoid copying files
    
    * When the files directory is missing on restore, report an error that refers to the new argument
    
    * Add --files-metadata-only to readme
    
    * Fix linting errors

--- nislmigrate/migrators/file_migrator.py
+++ nislmigrate/migrators/file_migrator.py
@@ -1,10 +1,11 @@
 import os
 
+from nislmigrate.extensibility.migrator_plugin import MigratorPlugin, ArgumentManager
 from nislmigrate.facades.facade_factory import FacadeFactory
-from nislmigrate.extensibility.migrator_plugin import MigratorPlugin
 from nislmigrate.facades.file_system_facade import FileSystemFacade
 from nislmigrate.facades.mongo_configuration import MongoConfiguration
 from nislmigrate.facades.mongo_facade import MongoFacade
+from nislmigrate.logs.migration_error import MigrationError
 from typing import Any, Dict
 
 DEFAULT_DATA_DIRECTORY = os.path.join(
@@ -15,6 +16,18 @@ DEFAULT_DATA_DIRECTORY = os.path.join(
     'FileIngestion')
 
 PATH_CONFIGURATION_KEY = 'OutputPath'
+
+_METADATA_ONLY_ARGUMENT = 'metadata-only'
+
+_METADATA_ONLY_HELP = 'When used with --files or --all, migrate only the file metadata, \
+but not the files themselves. Otherwise ignored.'
+
+_NO_FILES_ERROR = """
+
+Files data was not found. If you intend to restore metadata only, pass
+--files-metadata-only.
+
+"""
 
 
 class FileMigrator(MigratorPlugin):
@@ -41,10 +54,12 @@ class FileMigrator(MigratorPlugin):
             mongo_configuration,
             migration_directory,
             self.name)
-        file_facade.copy_directory(
-            self.__data_directory(facade_factory),
-            file_migration_directory,
-            False)
+
+        if self.__should_migrate_files(arguments):
+            file_facade.copy_directory(
+                self.__data_directory(facade_factory),
+                file_migration_directory,
+                False)
 
     def restore(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
@@ -56,10 +71,12 @@ class FileMigrator(MigratorPlugin):
             mongo_configuration,
             migration_directory,
             self.name)
-        file_facade.copy_directory(
-            file_migration_directory,
-            self.__data_directory(facade_factory),
-            True)
+
+        if self.__should_migrate_files(arguments):
+            file_facade.copy_directory(
+                file_migration_directory,
+                self.__data_directory(facade_factory),
+                True)
 
     def pre_restore_check(
             self,
@@ -71,5 +88,17 @@ class FileMigrator(MigratorPlugin):
             migration_directory,
             self.name)
 
+        if self.__should_migrate_files(arguments):
+            file_migration_directory = os.path.join(migration_directory, 'files')
+            file_facade = facade_factory.get_file_system_facade()
+            if not file_facade.migration_dir_exists(file_migration_directory):
+                raise MigrationError(_NO_FILES_ERROR)
+
+    def add_additional_arguments(self, argument_manager: ArgumentManager):
+        argument_manager.add_switch(_METADATA_ONLY_ARGUMENT, help=_METADATA_ONLY_HELP)
+
     def __data_directory(self, facade_factory):
         return self.config(facade_factory).get(PATH_CONFIGURATION_KEY, DEFAULT_DATA_DIRECTORY)
+
+    def __should_migrate_files(self, arguments):
+        return not arguments.get(_METADATA_ONLY_ARGUMENT)
