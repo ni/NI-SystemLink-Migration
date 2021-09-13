64dd57bc5a37ad9454924f76641b6ab9681cd09a
JD Robertson <44848933+JD-Robertson@users.noreply.github.com>
2021-09-13 13:54:05 -0500

    Consolidate existing fakes into TestUtilities (#85)
    * Fix flak8. mypy still not working.
    
    * Restart web server after restore
    
    * Revert "Fix flak8. mypy still not working."
    
    This reverts commit 13ee29cd368fb9ce16d8c34b73d4573e0c493698.
    
    * Fix linter issue from merge.
    
    * Get rid of duplicate fakes.
    
    * Cleanup test utilities
    
    * Fix linting issues
    
    * Remove default FileIngestion config from the FileSystemFacade fake
    
    * Remove debugging print.
    
    * Restore path validation to the run migrator tests.
    
    * Add more explicit assertions around path verification.

--- test/migrators/test_file_migrator.py
+++ test/migrators/test_file_migrator.py
@@ -1,4 +1,3 @@
-from nislmigrate.facades.file_system_facade import FileSystemFacade
 from nislmigrate.logs.migration_error import MigrationError
 from nislmigrate.migrators.file_migrator import (
     FileMigrator,
@@ -10,7 +9,7 @@ from nislmigrate.migrators.file_migrator import (
     _CANNOT_MIGRATE_S3_FILES_ERROR,
 )
 import pytest
-from test.test_utilities import FakeFacadeFactory
+from test.test_utilities import FakeFacadeFactory, FakeFileSystemFacade
 from typing import Any, Dict, Optional, Tuple
 
 
@@ -76,7 +75,7 @@ def test_file_migrator_does_not_capture_files_when_metadata_only_is_passed():
 @pytest.mark.unit
 def test_file_migrator_does_not_restore_files_when_metadata_only_is_passed():
 
-    facade_factory, file_system_facade = configure_facade_factory(dir_exists=False)
+    facade_factory, file_system_facade = configure_facade_factory(directory_exists=False)
     migrator = FileMigrator()
 
     migrator.restore('data_dir', facade_factory, {_METADATA_ONLY_ARGUMENT: True})
@@ -88,7 +87,7 @@ def test_file_migrator_does_not_restore_files_when_metadata_only_is_passed():
 @pytest.mark.parametrize('use_s3_backend', [(None), (False)])
 def test_file_migrator_reports_error_if_no_files_to_restore_and_not_metdata_only(use_s3_backend):
 
-    facade_factory, _ = configure_facade_factory(dir_exists=False, enable_s3_backend=use_s3_backend)
+    facade_factory, _ = configure_facade_factory(directory_exists=False, enable_s3_backend=use_s3_backend)
     migrator = FileMigrator()
 
     with pytest.raises(MigrationError) as e:
@@ -135,7 +134,7 @@ def test_file_migrator_pre_capture_check_reports_error_when_s3_backend_is_enable
 
 @pytest.mark.unit
 def test_file_migrator_pre_restore_check_reports_error_when_s3_backend_is_enabled_without_ignore_metadata_argument():
-    facade_factory, _ = configure_facade_factory(enable_s3_backend=True, dir_exists=False)
+    facade_factory, _ = configure_facade_factory(enable_s3_backend=True, directory_exists=False)
     migrator = FileMigrator()
 
     with pytest.raises(MigrationError) as e:
@@ -144,57 +143,49 @@ def test_file_migrator_pre_restore_check_reports_error_when_s3_backend_is_enable
     assert _CANNOT_MIGRATE_S3_FILES_ERROR in str(e.value)
 
 
-class FakeFileSystemFacade(FileSystemFacade):
-    def __init__(
-        self,
-        data_directory: Optional[str] = None,
-        null_data_directory: bool = False,
-        enable_s3_backend: Optional[bool] = None,
-        dir_exists: bool = True,
-    ):
-        self.last_from_directory: Optional[str] = None
-        self.last_to_directory: Optional[str] = None
-
-        self.config: Dict[str, Any] = {
-                'Mongo.CustomConnectionString': 'mongodb://localhost',
-                'Mongo.Database': 'file'
-            }
-
-        if null_data_directory:
-            self.config[PATH_CONFIGURATION_KEY] = None
-        elif data_directory is not None:
-            self.config[PATH_CONFIGURATION_KEY] = data_directory
-
-        if enable_s3_backend is not None:
-            self.config[S3_CONFIGURATION_KEY] = str(enable_s3_backend)
-
-        self.dir_exists = dir_exists
-
-    def copy_directory(self, from_directory: str, to_directory: str, force: bool):
-        self.last_from_directory = from_directory
-        self.last_to_directory = to_directory
-
-    def read_json_file(self, path: str) -> dict:
-        return {'FileIngestion': self.config}
-
-    def migration_dir_exists(self, dir_):
-        return self.dir_exists
-
-
 def configure_facade_factory(
     data_directory: Optional[str] = None,
     null_data_directory: bool = False,
     enable_s3_backend: Optional[bool] = None,
-    dir_exists: bool = True,
+    directory_exists: bool = True,
 ) -> Tuple[FakeFacadeFactory, FakeFileSystemFacade]:
-    file_system_facade = FakeFileSystemFacade(
+    facade_factory = FakeFacadeFactory()
+    file_system_facade = configure_fake_file_system_facade(
+        facade_factory,
         data_directory=data_directory,
         null_data_directory=null_data_directory,
         enable_s3_backend=enable_s3_backend,
-        dir_exists=dir_exists,
+        directory_exists=directory_exists,
     )
 
-    facade_factory = FakeFacadeFactory()
-    facade_factory.file_system_facade = file_system_facade
-
     return (facade_factory, file_system_facade)
+
+
+def configure_fake_file_system_facade(
+    facade_factory: FakeFacadeFactory,
+    data_directory: Optional[str] = None,
+    null_data_directory: bool = False,
+    enable_s3_backend: Optional[bool] = None,
+    directory_exists: bool = True,
+) -> FakeFileSystemFacade:
+    file_system_facade = facade_factory.file_system_facade
+    file_system_facade.directory_exists = directory_exists
+
+    properties: Dict[str, Any] = {
+            'Mongo.CustomConnectionString': 'mongodb://localhost',
+            'Mongo.Database': 'file'
+        }
+
+    if null_data_directory:
+        properties[PATH_CONFIGURATION_KEY] = None
+    if data_directory is not None:
+        properties[PATH_CONFIGURATION_KEY] = data_directory
+
+    if enable_s3_backend is not None:
+        properties[S3_CONFIGURATION_KEY] = str(enable_s3_backend)
+
+    file_system_facade.config = {
+            'FileIngestion': properties
+        }
+
+    return file_system_facade
