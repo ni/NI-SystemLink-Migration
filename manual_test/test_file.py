01801f71a31372559950a6a9ed9a84092a4d1769
Chris Pawling <chris.pawling@ni.com>
2021-09-13 17:03:35 -0500

    There may be pre-existing files, don't assume there aren't any

--- manual_test/test_file.py
+++ manual_test/test_file.py
@@ -13,7 +13,6 @@ get_route = '/nifile/v1/service-groups/Default/files'
 class TestFile(ManualTestBase):
 
     def populate_data(self):
-        self.__raise_if_existing_data()
         WorkspaceUtilities().create_workspace('WorkspaceForManualFilesMigrationTest', self)
         workspaces = WorkspaceUtilities().get_workspaces(self)
         self.__upload_files(workspaces)
@@ -25,21 +24,10 @@ class TestFile(ManualTestBase):
     def validate_data(self):
         workspaces = WorkspaceUtilities().get_workspaces(self)
         expected_files = self.__get_expected_files(workspaces)
-        data = self.__get_files(len(expected_files))
-        self.__assert_file_count(data, len(expected_files))
+        files = self.__get_files()
 
-        actual_files = self.__extract_file_details(data)
+        actual_files = self.__extract_file_details(files)
         self.__assert_files_match(actual_files, expected_files)
-
-    def __raise_if_existing_data(self):
-        response = self.get(get_route, params={'take': 1})
-        if response.status_code == 404:
-            return
-
-        response.raise_for_status()
-        data = response.json()
-        if data['availableFiles']:
-            raise RuntimeError('There is exising file data on the server')
 
     def __upload_files(self, workspaces):
         file_data = self.__get_expected_files(workspaces)
@@ -52,23 +40,33 @@ class TestFile(ManualTestBase):
             response = self.post(upload_route, params={'workspace': workspace}, files=file)
             response.raise_for_status()
 
-    def __get_files(self, expected_count):
-        response = self.get(get_route, params={'take': expected_count})
-        response.raise_for_status()
+    def __get_files(self):
+        take = 100
+        skip = 0
+        count = take
+        all_files = []
+        while count == take:
+            response = self.get(get_route, params={'take': take, 'skip': skip})
+            response.raise_for_status()
 
-        return response.json()
+            received_files = response.json()['availableFiles']
+            count = len(received_files)
+            all_files.extend(received_files)
 
-    def __extract_file_details(self, data) -> List[Dict[str, Any]]:
-        available_files = data['availableFiles']
-        return [self.__extract_single_file_details(available_file)
-                for available_file in available_files]
+            skip += take
 
-    def __extract_single_file_details(self, available_file) -> Dict[str, Any]:
-        data_url = available_file['_links']['data']['href']
+        return all_files
+
+    def __extract_file_details(self, files) -> List[Dict[str, Any]]:
+        return [self.__extract_single_file_details(file)
+                for file in files]
+
+    def __extract_single_file_details(self, file) -> Dict[str, Any]:
+        data_url = file['_links']['data']['href']
         contents = self.__download_file_contents(data_url)
-        filename = available_file['properties']['Name']
-        properties = {k: v for k, v in available_file['properties'].items() if k != 'Name'}
-        workspace = available_file['workspace']
+        filename = file['properties']['Name']
+        properties = {k: v for k, v in file['properties'].items() if k != 'Name'}
+        workspace = file['workspace']
         return {'filename': filename, 'contents': contents, 'properties': properties, 'workspace': workspace}
 
     def __download_file_contents(self, url):
@@ -78,16 +76,9 @@ class TestFile(ManualTestBase):
         return response.text
 
     @staticmethod
-    def __assert_file_count(data, expected_count):
-        total_files = data['totalCount']
-        actual_files = data['availableFiles']
-
-        assert total_files == expected_count
-        assert len(actual_files) == expected_count
-
-    @staticmethod
     def __assert_files_match(actual_files, expected_files):
         by_filename = lambda i : i['filename']
+        assert len(actual_files) == len(expected_files)
         assert sorted(actual_files, key=by_filename) == sorted(expected_files, key=by_filename)
 
     @staticmethod
