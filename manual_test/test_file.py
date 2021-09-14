e0244c6e1eb19e38f8729406eb4ce54186b0f855
Chris Pawling <chris.pawling@ni.com>
2021-09-14 15:03:03 -0500

    Add a tdms file

--- manual_test/test_file.py
+++ manual_test/test_file.py
@@ -12,6 +12,7 @@ GET_ROUTE = '/nifile/v1/service-groups/Default/files'
 
 ASSETS_PATH = Path(__file__).parent / 'assets'
 IMAGE_PATH = ASSETS_PATH / 'Image.png'
+TDMS_PATH = ASSETS_PATH / 'Data.tdms'
 
 SERVICE_NAME = 'Files'
 COLLECTION_NAME = 'FileIngestion'
@@ -106,13 +107,14 @@ class TestFile(ManualTestBase):
     def __get_files_to_create(workspaces) -> List[Dict[str, Any]]:
         files_specs: List[Dict[str, Any]] = []
         for i in range(len(workspaces)):
-            files_specs.append(TestFile.__create_text_file(i, workspaces[i]))
-            files_specs.append(TestFile.__create_png_file(i, workspaces[i]))
+            files_specs.append(TestFile.__create_text_file_spec(i, workspaces[i]))
+            files_specs.append(TestFile.__create_png_file_spec(i, workspaces[i]))
+            files_specs.append(TestFile.__create_tdms_file_spec(i, workspaces[i]))
 
         return files_specs
 
     @staticmethod
-    def __create_text_file(index: int, workspace: str) -> Dict[str, Any]:
+    def __create_text_file_spec(index: int, workspace: str) -> Dict[str, Any]:
         return {
                 'filename': f'File {index}.txt',
                 'inlineTextContents': f'Contents {index}',
@@ -123,12 +125,23 @@ class TestFile(ManualTestBase):
             }
 
     @staticmethod
-    def __create_png_file(index: int, workspace: str) -> Dict[str, Any]:
+    def __create_png_file_spec(index: int, workspace: str) -> Dict[str, Any]:
         return {
-                'filename': 'Image.png',
+                'filename': IMAGE_PATH.name,
                 'contentsFile': IMAGE_PATH,
                 'properties': {
                     f'image{index}': f'imageValue{index}'
+                },
+                'workspace': workspace
+            }
+
+    @staticmethod
+    def __create_tdms_file_spec(index: int, workspace: str) -> Dict[str, Any]:
+        return {
+                'filename': TDMS_PATH.name,
+                'contentsFile': TDMS_PATH,
+                'properties': {
+                    f'tdms{index}': f'tdmsValue{index}'
                 },
                 'workspace': workspace
             }
