e0c2894dafa5e935f32be52e28fbde3681f8aa51
JD Robertson <jd.robertson@ni.com>
2021-09-14 10:48:41 -0500

    Add validation

--- manual_test/test_system_states.py
+++ manual_test/test_system_states.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 from manual_test_base import ManualTestBase, handle_command_line, CLEAN_SERVER_RECORD_TYPE, POPULATED_SERVER_RECORD_TYPE
 from manual_test.utilities.workspace_utilities import WorkspaceUtilities
 
@@ -38,7 +38,46 @@ class TestSystemStates(ManualTestBase):
         )
 
     def validate_data(self) -> None:
-        pass
+        source_service_snapshot = self.read_recorded_data(
+            SERVICE_NAME,
+            SYSTEM_STATE_DATABASE_NAME,
+            POPULATED_SERVER_RECORD_TYPE,
+            required=True)
+        target_service_snaphot = self.read_recorded_data(
+            SERVICE_NAME,
+            SYSTEM_STATE_DATABASE_NAME,
+            CLEAN_SERVER_RECORD_TYPE,
+            required=False)
+        current_snapshot = self.__get_all_state_data()
+        current_states = current_snapshot['current_versions']
+        histories = current_snapshot['histories']
+        v1_states = current_snapshot['v1_states']
+        workspaces = WorkspaceUtilities().get_workspaces(self)
+
+        migrated_record_count = 0
+        for state in current_states:
+            expected_state = self.find_record_with_matching_id(state, source_service_snapshot['current_versions'])
+            if expected_state is not None:
+                self.__assert_states_equal(expected_state, state)
+                self.__assert_state_has_valid_workspace(state, workspaces)
+                self.__assert_histories_equal(
+                    state['id'],
+                    source_service_snapshot['histories'],
+                    histories
+                )
+                self.__assert_v1_states_equal(
+                    state['id'],
+                    source_service_snapshot['v1_states'],
+                    v1_states
+                )
+                migrated_record_count = migrated_record_count + 1
+            else:
+                # This test does not expect any auto-generated states. It just verifies that if any extra
+                # states exist, they were present when the server first started.
+                expected_state = self.__find_state_by_name(state, target_service_snaphot['current_versions'])
+                assert expected_state is not None
+
+        assert len(source_service_snapshot['current_versions']) == migrated_record_count
 
     def __get_all_state_data(self) -> Dict[str, Any]:
         all_state_ids = self.__get_all_state_ids()
@@ -141,6 +180,42 @@ class TestSystemStates(ManualTestBase):
         uri = PATCH_SYSTEM_STATE_ROUTE_FORMAT.format(id=state_id)
         response = self.patch(uri, json=patch)
         response.raise_for_status()
+
+    def __assert_states_equal(self, expected: Dict[str, Any], actual: Dict[str, Any]):
+        assert expected == actual
+
+    def __assert_state_has_valid_workspace(self, state: Dict[str, Any], workspaces: List[str]):
+        matching_workspace = next((workspace for workspace in workspaces if workspace == state['workspace']), None)
+        assert matching_workspace is not None
+
+    def __assert_histories_equal(
+        self,
+        state_id: str,
+        expected_histories: List[Dict[str, Any]],
+        actual_histories: List[Dict[str, Any]]
+    ):
+        actual_history = self.find_record_by_id(state_id, actual_histories)
+        assert actual_history is not None
+        expected_history = self.find_record_by_id(state_id, expected_histories)
+        assert actual_history == expected_history
+
+    def __assert_v1_states_equal(
+        self,
+        state_id: str,
+        expected_v1_states: List[Dict[str, Any]],
+        actual_v1_states: List[Dict[str, Any]]
+    ):
+        actual_state = self.find_record_by_id(state_id, actual_v1_states)
+        assert actual_state is not None
+        expected_state = self.find_record_by_id(state_id, expected_v1_states)
+        self.__assert_states_equal(expected_state, actual_state)
+
+    def __find_state_by_name(
+        self,
+        state: Dict[str, Any],
+        collection: List[Dict[str, Any]]
+    ) -> Optional[Dict[str, Any]]:
+        return self.find_record_with_matching_property_value(state, collection, 'name')
 
 
 if __name__ == '__main__':
