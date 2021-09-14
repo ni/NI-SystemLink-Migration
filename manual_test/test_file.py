81205ecf51e63e5eaee339906e94075cdfeef9de
Chris Pawling <chris.pawling@ni.com>
2021-09-14 17:18:24 -0500

    Different file types (#94)
    * Fix casing to follow conventions
    
    * Add a single text file per workspace
    
    * Upload a file
    
    * Add a tdms file

--- manual_test/test_file.py
+++ manual_test/test_file.py
@@ -1,12 +1,18 @@
+import base64
 import json
 
 from manual_test.utilities.workspace_utilities import WorkspaceUtilities
 from manual_test.manual_test_base import POPULATED_SERVER_RECORD_TYPE, ManualTestBase, handle_command_line
+from pathlib import Path
 from typing import Any, Dict, List
 
 
-upload_route = '/nifile/v1/service-groups/Default/upload-files'
-get_route = '/nifile/v1/service-groups/Default/files'
+UPLOAD_ROUTE = '/nifile/v1/service-groups/Default/upload-files'
+GET_ROUTE = '/nifile/v1/service-groups/Default/files'
+
+ASSETS_PATH = Path(__file__).parent / 'assets'
+IMAGE_PATH = ASSETS_PATH / 'Image.png'
+TDMS_PATH = ASSETS_PATH / 'Data.tdms'
 
 SERVICE_NAME = 'Files'
 COLLECTION_NAME = 'FileIngestion'
@@ -31,19 +37,28 @@ class TestFile(ManualTestBase):
         self.__assert_files_match(actual_files, expected_files)
 
     def __upload_files(self, workspaces):
-        file_data = self.__get_files_to_create(workspaces)
-        for data in file_data:
-            file = {
-                    'file': (data['filename'], data['contents']),
-                    'metadata': json.dumps(data['properties'])
+        file_specs = self.__get_files_to_create(workspaces)
+        for file_spec in file_specs:
+            upload: Dict[str, Any] = {
+                    'metadata': json.dumps(file_spec['properties'])
                    }
-            workspace = data['workspace']
-            response = self.post(
-                upload_route,
-                params={'workspace': workspace},
-                files=file,
-                retries=self.build_default_400_retry())
-            response.raise_for_status()
+            workspace = file_spec['workspace']
+
+            if file_spec.get('inlineTextContents', None):
+                upload['file'] = (file_spec['filename'], file_spec['inlineTextContents'])
+                self.__upload_file(upload, workspace)
+            else:
+                with open(file_spec['contentsFile'], 'rb') as contents:
+                    upload['file'] = (file_spec['filename'], contents)
+                    self.__upload_file(upload, workspace)
+
+    def __upload_file(self, upload, workspace):
+        response = self.post(
+            UPLOAD_ROUTE,
+            params={'workspace': workspace},
+            files=upload,
+            retries=self.build_default_400_retry())
+        response.raise_for_status()
 
     def __get_files(self):
         take = 100
@@ -51,7 +66,7 @@ class TestFile(ManualTestBase):
         count = take
         all_files = []
         while count == take:
-            response = self.get(get_route, params={'take': take, 'skip': skip})
+            response = self.get(GET_ROUTE, params={'take': take, 'skip': skip})
             response.raise_for_status()
 
             received_files = response.json()['availableFiles']
@@ -77,7 +92,7 @@ class TestFile(ManualTestBase):
         response = self.get(url)
         response.raise_for_status()
 
-        return response.text
+        return base64.b64encode(response.content).decode('utf-8')
 
     @staticmethod
     def __sorted_by_id(files):
@@ -90,20 +105,46 @@ class TestFile(ManualTestBase):
 
     @staticmethod
     def __get_files_to_create(workspaces) -> List[Dict[str, Any]]:
-        files: List[Dict[str, Any]] = []
+        files_specs: List[Dict[str, Any]] = []
         for i in range(len(workspaces)):
-            for j in range(10):
-                count = i * 10 + j
-                files.append({
-                    'filename': f'File {count}.txt',
-                    'contents': f'Contents {count}',
-                    'properties': {
-                        f'key{count}': f'value{count}'
-                    },
-                    'workspace': workspaces[i]
-                })
+            files_specs.append(TestFile.__create_text_file_spec(i, workspaces[i]))
+            files_specs.append(TestFile.__create_png_file_spec(i, workspaces[i]))
+            files_specs.append(TestFile.__create_tdms_file_spec(i, workspaces[i]))
 
-        return files
+        return files_specs
+
+    @staticmethod
+    def __create_text_file_spec(index: int, workspace: str) -> Dict[str, Any]:
+        return {
+                'filename': f'File {index}.txt',
+                'inlineTextContents': f'Contents {index}',
+                'properties': {
+                    f'key{index}': f'value{index}'
+                },
+                'workspace': workspace
+            }
+
+    @staticmethod
+    def __create_png_file_spec(index: int, workspace: str) -> Dict[str, Any]:
+        return {
+                'filename': IMAGE_PATH.name,
+                'contentsFile': IMAGE_PATH,
+                'properties': {
+                    f'image{index}': f'imageValue{index}'
+                },
+                'workspace': workspace
+            }
+
+    @staticmethod
+    def __create_tdms_file_spec(index: int, workspace: str) -> Dict[str, Any]:
+        return {
+                'filename': TDMS_PATH.name,
+                'contentsFile': TDMS_PATH,
+                'properties': {
+                    f'tdms{index}': f'tdmsValue{index}'
+                },
+                'workspace': workspace
+            }
 
     def __record_data(self, record_type: str):
         self.record_data(
