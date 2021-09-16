d33ad3357c734b0b9ada7a1b294c91a65e1471fa
Chris Pawling <chris.pawling@ni.com>
2021-09-16 14:05:48 -0500

    Add test class

---
+++ manual_test/test_testmonitor.py
@@ -0,0 +1,8 @@
+from manual_test.manual_test_base import ManualTestBase, handle_command_line
+
+class TestTestMonitor(ManualTestBase):
+    pass
+
+
+if __name__ == '__main__':
+    handle_command_line(TestTestMonitor)
