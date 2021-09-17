be171c15c4cf39f4aa90f657b136586aab42c8cb
Chris Pawling <chris.pawling@ni.com>
2021-09-17 10:39:22 -0500

    populate products

--- manual_test/test_test_monitor.py
+++ manual_test/test_test_monitor.py
@@ -4,12 +4,14 @@ from manual_test.utilities.workspace_utilities import WorkspaceUtilities
 from datetime import datetime, timedelta
 import random
 from random import randrange
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 from urllib.parse import urlparse
 
 RESULTS_ROUTE='/nitestmonitor/v2/results'
 STEPS_ROUTE='/nitestmonitor/v2/steps'
 PRODUCTS_ROUTE='/nitestmonitor/v2/products'
+
+CATEGORY='test_monitor'
 
 class TestTestMonitor(ManualTestBase):
     statuses = [
@@ -34,10 +36,10 @@ class TestTestMonitor(ManualTestBase):
         pass
 
     def validate_data(self):
-        expected_test_monitor_data = self.__read_recorded_data(POPULATED_SERVER_RECORD_TYPE)
-        actual_test_monitor_data = self.__get_test_monitor_data()
-
-        self.__assert_test_monitor_data_matches(actual_test_monitor_data, expected_test_monitor_data)
+        self.__validate_products()
+        self.__validate_results()
+        self.__validate_steps()
+        self.__validate_paths()
 
     def __populate_test_monitor_data(self, workspaces: List[str]):
         self.__create_products(workspaces[0])
@@ -45,21 +47,18 @@ class TestTestMonitor(ManualTestBase):
         # paths are automatically populated, there is not a write api
 
     def __create_products(self, workspace: str):
-        fileId = self.__upload_file(workspace)
-        products = [
-            {
+        products = []
+        for part in self.parts:
+            products.append({
                 'name': f'{part} Name',
                 'partNumber': part,
                 'family': f'{part}-family',
                 'keywords': [f'{part}-keyword-1', f'{part}-keyword-2'],
-                'properties': {f'{part}Property': f'{part}Value'},
-                'fileIds': [fileId]
-            }
-            for part in self.parts
-        ]
+                'properties': {f'{part}Property': f'{part}Value', 'forTest': 'True'},
+                'fileIds': [self.__upload_file(workspace)]
+            })
 
         response = self.post(PRODUCTS_ROUTE, json={'products': products})
-        print(response.json())
         response.raise_for_status()
 
     def __create_results_with_steps(self, workspaces: List[str]):
@@ -82,7 +81,10 @@ class TestTestMonitor(ManualTestBase):
             'programName': random.choice(programs), 'status': random.choice(self.statuses),
             'systemId': random.choice(systems),
             'hostName': random.choice(hosts),
-            'properties': {random.choice(propertyKeys): random.choice(propertyValues)},
+            'properties': {
+                random.choice(propertyKeys): random.choice(propertyValues),
+                'forTest': 'True'
+            },
             'keywords': [random.choice(keywords), random.choice(keywords)],
             'serialNumber': str(random.randint(111111, 999999)),
             'operator': random.choice(operators),
@@ -108,14 +110,13 @@ class TestTestMonitor(ManualTestBase):
             steps.append(step)
 
         response = self.post(STEPS_ROUTE, json={'steps': steps})
-        print(response.json())
         response.raise_for_status()
 
     def __create_step(self, result_id: str, name: str, index: int) -> Dict[str, Any]:
         return {
             'name': f'{name}{index}',
             'resultId': result_id,
-            'stepType': 'SequenceCall',
+            'stepType': 'forTest',
             'status': random.choice(self.statuses),
             'startedAt': self.__random_date(),
             'totalTimeInSeconds': random.randint(1, 120),
@@ -146,17 +147,62 @@ class TestTestMonitor(ManualTestBase):
         id = urlparse(uri).path.split('/').pop()
         return id
 
-    def __get_test_monitor_data(self):
-        raise NotImplemented
+    def __validate_products(self):
+        raise NotImplementedError
 
-    def __assert_test_monitor_data_matches(self, actual_test_monitor_data, expected_test_monitor_data):
-        raise NotImplemented
+    def __validate_results(self):
+        raise NotImplementedError
+
+    def __validate_steps(self):
+        raise NotImplementedError
+
+    def __validate_paths(self):
+        raise NotImplementedError
 
     def __record_data(self, record_type: str):
-        raise NotImplemented
+        self.__record_product_data(record_type)
+        self.__record_result_data(record_type)
+        self.__record_step_data(record_type)
+        self.__record_path_data(record_type)
+
+    def __record_product_data(self, record_type: str):
+        products = self.__get_product_data()
+        self.record_data(CATEGORY, 'products', record_type, products)
+
+    def __record_result_data(self, record_type: str):
+        raise NotImplementedError
+
+    def __record_step_data(self, record_type: str):
+        raise NotImplementedError
+
+    def __record_path_data(self, record_type: str):
+        raise NotImplementedError
+
+    def __get_product_data(self):
+        return self.__get_all_with_continuation_token(PRODUCTS_ROUTE, 'products')
+
+    def __get_all_with_continuation_token(self, route: str, data_key: str) -> List[Dict[str, Any]]:
+        data, continuation_token = self.__get_data_and_continuation_token(route, data_key, None)
+        while continuation_token:
+            additional_data, continuation_token = self.__get_data_and_continuation_token(route, data_key, continuation_token)
+            data.extend(additional_data)
+
+        return data
+
+    def __get_data_and_continuation_token(
+        self,
+        route: str,
+        data_key: str,
+        continuation_token: Optional[str]
+    ) -> Tuple[List[Dict[str, Any]], str]:
+        params = {} if not continuation_token else {'continuationToken': continuation_token}
+        response = self.get(route, params=params)
+        response.raise_for_status()
+        data = response.json()
+        return data[data_key], data.get('continuationToken', None)
 
     def __read_recorded_data(self, record_type: str):
-        raise NotImplemented
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
