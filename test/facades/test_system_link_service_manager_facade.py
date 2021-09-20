7fe6ee428bb60462458ac9cffb4efd7ef2ed1915
JD Robertson <jd.robertson@ni.com>
2021-09-20 14:55:52 -0500

    Update unit tests.

--- test/facades/test_system_link_service_manager_facade.py
+++ test/facades/test_system_link_service_manager_facade.py
@@ -8,52 +8,28 @@ from nislmigrate.facades.system_link_service_manager_facade import SystemLinkSer
 
 
 @pytest.mark.unit
-@patch('os.path.exists')
-def test_stop_all_system_link_services_when_configuration_tool_not_installed(exists) -> None:
-    service_manager = SystemLinkServiceManagerFacade()
-    exists.return_value = False
-
-    with pytest.raises(MigrationError):
-        service_manager.stop_all_system_link_services()
-
-
-@pytest.mark.unit
 @patch('subprocess.run')
-@patch('os.path.exists')
-def test_stop_all_system_link_services_when_configuration_tool_installed(exists: Mock, run: Mock) -> None:
+def test_stop_all_system_link_services_when_configuration_tool_installed(run: Mock) -> None:
     service_manager = SystemLinkServiceManagerFacade()
-    exists.return_value = True
 
     service_manager.stop_all_system_link_services()
 
     run.assert_called_with(
-        system_link_service_manager_facade.STOP_ALL_SERVICES_COMMAND,
+        system_link_service_manager_facade.STOP_SERVICE_MANAGER_COMMAND,
         check=True,
         capture_output=True,
     )
 
 
 @pytest.mark.unit
-@patch('os.path.exists')
-def test_start_all_system_link_services_when_configuration_tool_not_installed(exists) -> None:
-    service_manager = SystemLinkServiceManagerFacade()
-    exists.return_value = False
-
-    with pytest.raises(MigrationError):
-        service_manager.start_all_system_link_services()
-
-
-@pytest.mark.unit
 @patch('subprocess.run')
-@patch('os.path.exists')
-def test_start_all_system_link_services_when_configuration_tool_installed(exists: Mock, run: Mock) -> None:
+def test_start_all_system_link_services_when_configuration_tool_installed(run: Mock) -> None:
     service_manager = SystemLinkServiceManagerFacade()
-    exists.return_value = True
 
     service_manager.start_all_system_link_services()
 
     run.assert_called_with(
-        system_link_service_manager_facade.START_ALL_SERVICES_COMMAND,
+        system_link_service_manager_facade.START_SERVICE_MANAGER_COMMAND,
         check=True,
         capture_output=True,
     )
