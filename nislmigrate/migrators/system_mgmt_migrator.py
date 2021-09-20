143ddc506813ab78ec9e2fd45f5d1179b28d2abc
JD Robertson <jd.robertson@ni.com>
2021-09-20 09:13:20 -0500

    Copy PKI

--- nislmigrate/migrators/system_mgmt_migrator.py
+++ nislmigrate/migrators/system_mgmt_migrator.py
@@ -1,9 +1,17 @@
+from nislmigrate.facades.file_system_facade import FileSystemFacade
 from nislmigrate.facades.facade_factory import FacadeFactory
 from nislmigrate.facades.mongo_configuration import MongoConfiguration
 from nislmigrate.facades.mongo_facade import MongoFacade
 from nislmigrate.extensibility.migrator_plugin import MigratorPlugin
+import os
 from typing import Any, Dict
 
+PKI_PATH = os.path.join(
+    str(os.environ.get('ProgramData')),
+    'National Instruments',
+    'salt',
+    'conf',
+    'pki')
 
 class SystemsManagementMigrator(MigratorPlugin):
 
@@ -22,18 +30,32 @@ class SystemsManagementMigrator(MigratorPlugin):
     def capture(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
         mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
+        file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
+        file_migration_directory = os.path.join(migration_directory, 'files')
+
         mongo_facade.capture_database_to_directory(
             mongo_configuration,
             migration_directory,
             self.name)
+        file_facade.copy_directory_if_exists(
+            PKI_PATH,
+            file_migration_directory,
+            False)
 
     def restore(self, migration_directory: str, facade_factory: FacadeFactory, arguments: Dict[str, Any]):
         mongo_facade: MongoFacade = facade_factory.get_mongo_facade()
         mongo_configuration: MongoConfiguration = MongoConfiguration(self.config(facade_factory))
+        file_facade: FileSystemFacade = facade_factory.get_file_system_facade()
+        file_migration_directory = os.path.join(migration_directory, 'files')
+
         mongo_facade.restore_database_from_directory(
             mongo_configuration,
             migration_directory,
             self.name)
+        file_facade.copy_directory_if_exists(
+            PKI_PATH,
+            file_migration_directory,
+            False)
 
     def pre_restore_check(
             self,
