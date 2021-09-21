84d754ff3d48fc7d64528a01a51b2855d6e40967
Christian Nunnally <christiansnunnally@gmail.com>
2021-09-21 13:32:25 -0500

    Update mongo_configuration.py

--- nislmigrate/facades/mongo_configuration.py
+++ nislmigrate/facades/mongo_configuration.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 MONGO_HOST_NAME_CONFIGURATION_KEY = 'Mongo.Host'
 MONGO_DATABASE_NAME_CONFIGURATION_KEY = 'Mongo.Database'
@@ -13,25 +13,25 @@ class MongoConfiguration:
         self.service_config = service_config
 
     @property
-    def password(self) -> str:
+    def password(self) -> Optional[str]:
         return self.service_config.get(MONGO_PASSWORD_CONFIGURATION_KEY)
 
     @property
-    def user(self) -> str:
+    def user(self) -> Optional[str]:
         return self.service_config.get(MONGO_USER_CONFIGURATION_KEY)
 
     @property
-    def connection_string(self) -> str:
+    def connection_string(self) -> Optional[str]:
         return self.service_config.get(MONGO_CUSTOM_CONNECTION_STRING_CONFIGURATION_KEY)
 
     @property
-    def port(self) -> str:
+    def port(self) -> Optional[str]:
         return self.service_config.get(MONGO_PORT_NAME_CONFIGURATION_KEY)
 
     @property
-    def database_name(self) -> str:
+    def database_name(self) -> Optional[str]:
         return self.service_config.get(MONGO_DATABASE_NAME_CONFIGURATION_KEY)
 
     @property
-    def host_name(self) -> str:
+    def host_name(self) -> Optional[str]:
         return self.service_config.get(MONGO_HOST_NAME_CONFIGURATION_KEY)
