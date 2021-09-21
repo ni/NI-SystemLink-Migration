573e1dba04025e7c9013f8e0904b21a67b950388
Christian Nunnally <christiansnunnally@gmail.com>
2021-09-21 13:24:39 -0500

    use .get() on instead of [] for dictionary access.
    use .get( on dictionary so that MongoConfiguration properties return correct value

--- nislmigrate/facades/mongo_configuration.py
+++ nislmigrate/facades/mongo_configuration.py
@@ -14,24 +14,24 @@ class MongoConfiguration:
 
     @property
     def password(self) -> str:
-        return self.service_config[MONGO_PASSWORD_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_PASSWORD_CONFIGURATION_KEY)
 
     @property
     def user(self) -> str:
-        return self.service_config[MONGO_USER_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_USER_CONFIGURATION_KEY)
 
     @property
     def connection_string(self) -> str:
-        return self.service_config[MONGO_CUSTOM_CONNECTION_STRING_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_CUSTOM_CONNECTION_STRING_CONFIGURATION_KEY)
 
     @property
     def port(self) -> str:
-        return self.service_config[MONGO_PORT_NAME_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_PORT_NAME_CONFIGURATION_KEY)
 
     @property
     def database_name(self) -> str:
-        return self.service_config[MONGO_DATABASE_NAME_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_DATABASE_NAME_CONFIGURATION_KEY)
 
     @property
     def host_name(self) -> str:
-        return self.service_config[MONGO_HOST_NAME_CONFIGURATION_KEY]
+        return self.service_config.get(MONGO_HOST_NAME_CONFIGURATION_KEY)
