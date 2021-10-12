7c841f90741cb4137d68ede8273c8c28f1b5d734
JD Robertson <jd.robertson@ni.com>
2021-10-12 09:58:15 -0500

    Add utility for the users collection.

---
+++ manual_test/utilities/user_utilities.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict, List
+
+from manual_test.manual_test_base import ManualTestBase
+
+
+QUERY_USER_ROUTE = '/niuser/v1/users/query'
+
+class UserUtilities:
+
+    def get_all_users(self, test: ManualTestBase) -> List[Dict[str, Any]]:
+        query = {}
+        return test.query_all_with_continuation_token(QUERY_USER_ROUTE, query, 'users')
