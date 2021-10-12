2c0c75faadc537d2fe70d5265a3df49ce02fc552
JD Robertson <jd.robertson@ni.com>
2021-10-12 09:58:28 -0500

    Add validation for dashboards.

--- manual_test/test_document_manager.py
+++ manual_test/test_document_manager.py
@@ -8,6 +8,7 @@ from manual_test.manual_test_base import \
     handle_command_line, \
     CLEAN_SERVER_RECORD_TYPE, \
     POPULATED_SERVER_RECORD_TYPE
+from manual_test.utilities.user_utilities import UserUtilities
 from manual_test.utilities.workspace_utilities import WorkspaceUtilities
 
 SERVICE_NAME = 'DocumentManager'
@@ -39,19 +40,58 @@ class TestDocumentManager(ManualTestBase):
         self.__record_all_web_apps(CLEAN_SERVER_RECORD_TYPE)
 
     def validate_data(self) -> None:
-        pass
+        source_app_snapshot = self.read_recorded_json_data(
+            SERVICE_NAME,
+            DOCUMENT_MANAGER_DATABASE_NAME,
+            POPULATED_SERVER_RECORD_TYPE,
+            required=True)
+        source_content_snapshot = self.read_recorded_json_data(
+            SERVICE_NAME,
+            DOCUMENT_MANAGER_CONTENT_COLLECTION_NAME,
+            POPULATED_SERVER_RECORD_TYPE,
+            required=True)
+        target_app_snapshot = self.read_recorded_json_data(
+            SERVICE_NAME,
+            DOCUMENT_MANAGER_DATABASE_NAME,
+            CLEAN_SERVER_RECORD_TYPE,
+            required=False)
+        current_app_snapshot = self.__get_all_web_apps()
+        current_content_snapshot = self.__get_all_content_by_app(current_app_snapshot)
+
+        current_workspace_ids = WorkspaceUtilities().get_workspaces(self)
+        current_users = UserUtilities().get_all_users(self)
+
+        test_app_count = 0
+        for app in current_app_snapshot:
+            if self.__is_test_app(app):
+                expected_app = self.find_record_with_matching_id(app, source_app_snapshot)
+                assert expected_app == app
+                self.__assert_has_valid_links(app, current_workspace_ids, current_users)
+                self.__assert_has_expected_content(
+                    app,
+                    source_content_snapshot,
+                    current_content_snapshot)
+                test_app_count = test_app_count + 1
+            else:
+                # Built-in dashboards may vary by version. Just make sure the item
+                # comes from somewhere.
+                source_app = self.__find_by_name(app, source_app_snapshot)
+                target_app = self.__find_by_name(app, target_app_snapshot)
+                assert source_app is not None or target_app is not None
+
+        assert test_app_count > 0
 
     def __record_all_web_apps(self, record_type: str):
         all_apps = self.__get_all_web_apps()
         self.record_json_data(
             SERVICE_NAME,
             DOCUMENT_MANAGER_DATABASE_NAME,
-            CLEAN_SERVER_RECORD_TYPE,
+            record_type,
             all_apps)
         self.record_json_data(
             SERVICE_NAME,
             DOCUMENT_MANAGER_CONTENT_COLLECTION_NAME,
-            CLEAN_SERVER_RECORD_TYPE,
+            record_type,
             self.__get_all_content_by_app(all_apps))
 
     def __get_all_web_apps(self) -> List[Dict[str, Any]]:
@@ -101,6 +141,40 @@ class TestDocumentManager(ManualTestBase):
         with open(DASHBOARD_CONTENT_FILE_PATH, 'rb') as file:
             response = self.put(uri, data=file)
             response.raise_for_status()
+
+    def __assert_has_valid_links(
+        self,
+        app: Dict[str, Any],
+        current_workspace_ids: List[str],
+        current_users: List[Dict[str, Any]]
+    ):
+        # Workspace may empty if the dashboard is not workspace-restricted
+        workspace_id = app['workspace']
+        if workspace_id != '':
+            assert workspace_id in current_workspace_ids
+
+        user = self.find_record_by_id(app['userId'], current_users)
+        assert user is not None
+
+    def __assert_has_expected_content(
+        self,
+        app: Dict[str, Any],
+        source_content_snapshot: Dict[str, Any],
+        current_content_snapshot: Dict[str, Any]
+    ):
+        expected_content = self.find_record_by_property_value(app['id'], source_content_snapshot, 'appId')
+        actual_content = self.find_record_by_property_value(app['id'], current_content_snapshot, 'appId')
+        assert expected_content == actual_content
+
+    def __is_test_app(self, app: Dict[str, Any]) -> bool:
+        return 'forTest' in app['properties']
+
+    def __find_by_name(
+        self,
+        app: Dict[str, Any],
+        collection: List[Dict[str, Any]]
+    ) -> Optional[Dict[str, Any]]:
+        return self.find_record_with_matching_property_value(app, collection, 'name')
 
 
 if __name__ == '__main__':
