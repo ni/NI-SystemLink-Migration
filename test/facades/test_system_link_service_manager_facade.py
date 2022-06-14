1c9f38231741fa419d0b23c98506a80fae7945d4
Christian Nunnally <christian.nunnally@ni.com>
2022-06-14 14:01:15 -0500

    linted

--- test/facades/test_system_link_service_manager_facade.py
+++ test/facades/test_system_link_service_manager_facade.py
@@ -35,20 +35,20 @@ def test_start_all_system_link_services_when_configuration_tool_installed(run: M
         capture_output=True,
     )
 
+
 @pytest.mark.unit
 @patch('subprocess.run')
 def test_stop_all_system_link_services_succeeds_when_services_already_stopped_error_occurs(run: Mock) -> None:
-    run.side_effect = subprocess.CalledProcessError(returncode=2, cmd=["cmd"])
-
+    run.side_effect = subprocess.CalledProcessError(returncode=2, cmd=['cmd'])
     service_manager = SystemLinkServiceManagerFacade()
 
     service_manager.stop_all_system_link_services()
 
+
 @pytest.mark.unit
 @patch('subprocess.run')
 def test_stop_all_system_link_services_fails_when_other_error_occurs(run: Mock) -> None:
-    run.side_effect = subprocess.CalledProcessError(returncode=1, cmd=["cmd"])
-
+    run.side_effect = subprocess.CalledProcessError(returncode=1, cmd=['cmd'])
     service_manager = SystemLinkServiceManagerFacade()
 
     with pytest.raises(MigrationError):
